d6b1081000e665460d044e5da54aa464ea498a05
Bhaskar <bhaskar.anil429@gmail.com>
2020-02-21 12:25:28 -0600

    Add files via upload

---
+++ ESP32_Wireless_tilt_sensor.ino
@@ -0,0 +1,77 @@
+
+/// sesnro can be found here https://store.ncd.io/
+
+  uint8_t data[29];
+  int k = 10;
+  int i;
+
+void setup()
+{
+  Serial1.begin(115200, SERIAL_8N1, 16, 17); // pins 16 rx2, 17 tx2, 19200 bps, 8 bits no parity 1 stop bit​
+  Serial.begin(9600);
+  Serial.println("ncd.io IoT Wireelss Tilt Sensor");
+}
+
+
+void loop()
+{
+  
+  if (Serial1.available())
+  {
+    data[0] = Serial1.read();
+    delay(k);
+   if(data[0]==0x7E)
+    {
+    while (!Serial1.available());
+    for ( i = 1; i< 29; i++)
+      {
+      data[i] = Serial1.read();
+      delay(1);
+      }
+    if(data[15]==0x7F)  /////// to check if the recive data is correct
+      {
+  if(data[22]==47)  //////// make sure the sensor type is correct
+         {  
+  float roll_tilt = ((int16_t)(((data[24]) * 256) + data[25]) /100.0);
+  float pitch_tilt = ((int16_t)(((data[26])<<8)| data[27])/100.0);
+  float battery = ((data[18] * 256) + data[19]);
+  float voltage = 0.00322 * battery;
+  Serial.print("Wireless Packet Counter  ");
+  Serial.println(data[20]);
+  Serial.print("Sensor Number  ");
+  Serial.println(data[16]);
+  Serial.print("Sensor Type  ");
+  Serial.println(data[22]);
+  Serial.print("Firmware Version  ");
+  Serial.println(data[17]);
+  Serial.print("roll_tilt:");
+  Serial.print(roll_tilt,2);
+  Serial.println(" 'Degree");
+  Serial.print("pitch_tilt :");
+  Serial.print(pitch_tilt,2);
+  Serial.println(" 'degree");
+  Serial.print("ADC value:");
+  Serial.println(battery);
+  Serial.print("Battery Voltage:");
+  Serial.print(voltage);
+  Serial.println("\n");
+  if (voltage < 1)
+          {
+    Serial.println("Time to Replace The Battery");
+          }
+  
+        }
+      }
+else
+{
+      for ( i = 0; i< 29; i++)
+    {
+      Serial.print(data[i]);
+      Serial.print(" , ");
+      delay(1);
+    }
+}
+    }
+  }
+ 
+}
